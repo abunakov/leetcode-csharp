0ea4e861ae62f895ca6d45084426f83546a20998
Alexander Bunakov <abunakov@users.noreply.github.com>
2023-10-06 15:37:01 +0300

    Added TopInterview150 and TopInterview150Tests projects. Implemented MergeSortArray -- first task solution, with test coverage.

---
+++ LeetCode/TopInterview150/ArrayString/MergeSortedArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopInterview150.ArrayString
+{
+    public class MergeSortedArray
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            // point to the last element of each array
+            int pend = m + n;
+            m--;
+            n--;
+
+            while (n >= 0)
+            {
+                --pend;
+                if (m >= 0)
+                {
+                    if (nums1[m] <= nums2[n])
+                    {
+                        nums1[pend] = nums2[n--];
+                    }
+                    else // (nums1[m] > nums2[n])
+                    {
+                        nums1[pend] = nums1[m--];
+                    }
+                }
+                else
+                {
+                    nums1[pend] = nums2[n--];
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode/TopInterview150Tests/ArrayString/MergeSortedArrayTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TopInterview150.ArrayString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopInterview150.ArrayString.Tests
+{
+    [TestClass()]
+    public class MergeSortedArrayTests
+    {
+        [TestMethod()]
+        public void Merge_Test1()
+        {
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            const int m = 3;
+            int[] nums2 = { 2, 5, 6 };
+            const int n = 3;
+            int[] expected = { 1, 2, 2, 3, 5, 6 };
+
+            var solution = new MergeSortedArray();
+            solution.Merge(nums1, m, nums2, n);
+
+            Assert.IsTrue(Enumerable.SequenceEqual(expected, nums1));
+        }
+
+        [TestMethod()]
+        public void Merge_Test2()
+        {
+            int[] nums1 = { 1 };
+            const int m = 1;
+            int[] nums2 = Array.Empty<int>();
+            const int n = 0;
+            int[] expected = { 1 };
+
+            var solution = new MergeSortedArray();
+            solution.Merge(nums1, m, nums2, n);
+
+            Assert.IsTrue(Enumerable.SequenceEqual(expected, nums1));
+        }
+
+        [TestMethod()]
+        public void Merge_Test3()
+        {
+            int[] nums1 = { 0 };
+            const int m = 0;
+            int[] nums2 = { 1 };
+            const int n = 1;
+            int[] expected = { 1 };
+
+            var solution = new MergeSortedArray();
+            solution.Merge(nums1, m, nums2, n);
+
+            Assert.IsTrue(Enumerable.SequenceEqual(expected, nums1));
+        }
+    }
+}
