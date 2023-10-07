e137b2c7117bf8e3672bfc3e3eb241c18b3bfaf6
Alexander Bunakov <abunakov@users.noreply.github.com>
2023-10-07 13:41:42 +0300

    Added RemoveElement solution.

---
+++ LeetCode/TopInterview150/ArrayString/RemoveElementSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopInterview150.ArrayString
+{
+    public class RemoveElementSolution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+            int p1 = 0;
+            int p2 = nums.Length - 1;
+
+            while (p1 <= p2)
+            {
+                if (nums[p2] == val)
+                {
+                    p2--;
+                    continue;
+                }
+                if (nums[p1] == val)
+                {
+                    nums[p1] = nums[p2];
+                    p2--;
+                }
+                p1++;
+                k++;
+            }
+
+            return k;
+        }
+    }
+}
---
+++ LeetCode/TopInterview150Tests/ArrayString/RemoveElementSolutionTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TopInterview150.ArrayString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopInterview150.ArrayString.Tests
+{
+    [TestClass()]
+    public class RemoveElementSolutionTests
+    {
+        [TestMethod()]
+        public void RemoveElement_Test1()
+        {
+            int[] nums = { 3, 2, 2, 3 }; // Input array
+            int val = 3; // Value to remove
+            int[] expectedNums = { 2, 2 }; // The expected answer with correct length.
+                                        // It is sorted with no values equaling val.
+            
+            var solution = new RemoveElementSolution();
+            int k = solution.RemoveElement(nums, val); // Calls your implementation
+
+            Assert.AreEqual(expectedNums.Length, k);
+
+            //Array.Sort(nums, 0, k); // Sort the first k elements of nums
+            for (int i = 0; i < k; i++)
+            {
+                Assert.AreEqual(expectedNums[i], nums[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void RemoveElement_Test2()
+        {
+            int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 }; // Input array
+            int val = 2; // Value to remove
+            int[] expectedNums = { 0, 1, 4, 0, 3 }; // The expected answer with correct length.
+                                           // It is sorted with no values equaling val.
+
+            var solution = new RemoveElementSolution();
+            int k = solution.RemoveElement(nums, val); // Calls your implementation
+
+            Assert.AreEqual(expectedNums.Length, k);
+
+            //Array.Sort(nums, 0, k); // Sort the first k elements of nums
+            for (int i = 0; i < k; i++)
+            {
+                Assert.AreEqual(expectedNums[i], nums[i]);
+            }
+        }
+    }
+}
