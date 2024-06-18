c876cdf72be31aaa0bdc46d192a7beec28c3adc6
Alexander Bunakov <abunakov@users.noreply.github.com>
2024-06-18 08:46:18 +0300

    Remove Duplicates from Sorted Array
    Added a solution to "26. Remove Duplicates from Sorted Array"

---
+++ LeetCode/TopInterview150/ArrayString/RemoveDuplicatesFromSortedArraySolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopInterview150.ArrayString
+{
+    public class RemoveDuplicatesFromSortedArraySolution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return 1;
+
+            int k = 1;
+            int p1 = 0;
+            int p2 = 1;
+            while (p2 < nums.Length)
+            {
+                if (nums[p1] == nums[p2])
+                {
+                    p2++;
+                    continue;
+                }
+                nums[++p1] = nums[p2++];
+                k++;
+            }
+            return k;
+        }
+    }
+}
---
+++ LeetCode/TopInterview150Tests/ArrayString/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TopInterview150.ArrayString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopInterview150Tests.ArrayString
+{
+    [TestClass()]
+    public class RemoveDuplicatesFromSortedArrayTests
+    {
+        [TestMethod()]
+        public void RemoveDuplicates_Example1()
+        {
+            int[] nums = { 1, 1, 2 }; // Input array
+            int[] expectedNums = { 1, 2 }; // The expected answer with correct length
+
+            var solution = new RemoveDuplicatesFromSortedArraySolution();
+            int k = solution.RemoveDuplicates(nums); // Calls your implementation
+
+            Assert.IsTrue( k == expectedNums.Length);
+            for (int i = 0; i < k; i++)
+            {
+                Assert.AreEqual<int>(expectedNums[i], nums[i]);
+            }
+        }
+
+        [TestMethod]
+        public void RemoveDuplicates_Example2()
+        {
+            int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }; // Input array
+            int[] expectedNums = { 0, 1, 2, 3, 4 }; // The expected answer with correct length
+
+            var solution = new RemoveDuplicatesFromSortedArraySolution();
+            int k = solution.RemoveDuplicates(nums); // Calls your implementation
+
+            Assert.IsTrue(k == expectedNums.Length);
+            for (int i = 0; i < k; i++)
+            {
+                Assert.AreEqual<int>(expectedNums[i], nums[i]);
+            }
+        }
+    }
+}
